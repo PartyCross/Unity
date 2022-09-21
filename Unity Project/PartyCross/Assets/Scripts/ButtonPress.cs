217617ca63bcef9db17a4f0e7a6debc30a049787
NZFoxy <84160506+NZFoxy@users.noreply.github.com>
2022-09-22 11:19:01 +1200

    Buttons Functioning
    Buttons are functioning and able to access scripts in the game.

---
+++ Unity Project/PartyCross/Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    public void test()
+    {
+        Debug.Log("Button was pressed.");
+    }
+}
